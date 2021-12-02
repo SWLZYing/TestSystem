2251e751d0a9feb0ea266650ef84e71b9bacc43d
I552\Administrator <aa>
2021-12-02 11:12:12 +0800

    加入DB物件

---
+++ TestSystem.Webapi/Models/TestSystemContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TestSystem.Webapi.Models.Tables;
+
+namespace TestSystem.Webapi.Models
+{
+    public class TestSystemContext : DbContext
+    {
+        public TestSystemContext() : base("TestSystemContext")
+        {
+        }
+
+        public DbSet<T_Account> T_Accounts { get; set; }
+        public DbSet<T_AccountLevel> T_AccountLevels { get; set; }
+        public DbSet<T_LoginRecord> T_LoginRecords { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
