5774d892c26a526d609127d5696564851c40a7fd
SWLZYing <swlzying@gmail.com>
2021-12-06 17:39:42 +0800

    調整登入SP

--- TestSystem.DataBase/dbo/Stored Procedures/pro_signIn.sql
+++ TestSystem.DataBase/dbo/Stored Procedures/pro_signIn.sql
@@ -2,6 +2,11 @@
 	描述: 登入
 	建立日期: 2021-12-03
 
+	狀態 
+		0: 查無帳號
+		1: 登入成功
+		2: 密碼錯誤
+
 	exec pro_signIn 
 		@acc = 'USER001', 
 		@pwd = 'PWD001'
@@ -11,15 +16,37 @@ CREATE PROCEDURE [dbo].[pro_signIn]
 @acc NVARCHAR(20),
 @pwd NVARCHAR(50)
 AS
-	DECLARE @id INT
+	DECLARE @dbId INT
+	DECLARE @dbPwd NVARCHAR(50)
 
-	SELECT @id = f_id 
+	SELECT @dbId = f_id, @dbPwd = f_password
 	  FROM t_account WITH(NOLOCK)
 	 WHERE f_account = @acc
-	   AND f_password = @pwd
 
-    INSERT INTO t_loginRecord(f_accountId, f_loginTime, f_createTime, f_updateTime)
-	VALUES (@id, GETDATE(), GETDATE(), GETDATE())
+	IF @dbId IS NULL
+	BEGIN
+		SELECT 0
+	END
+	ELSE
+	BEGIN
+		IF @dbPwd != @pwd
+		BEGIN
+			SELECT 2
+		END
+		ELSE
+		BEGIN
+			SELECT 1
+
+			UPDATE t_account WITH(ROWLOCK)
+			   SET f_lastLoginTime = GETDATE() 
+			     , f_updateTime = GETDATE()
+			OUTPUT inserted.*
+			 WHERE f_id = @dbId
+
+			INSERT INTO t_loginRecord(f_accountId, f_loginTime, f_createTime, f_updateTime)
+			VALUES (@dbId, GETDATE(), GETDATE(), GETDATE())
+		END
+	END
 RETURN 0
 GO
 GRANT EXECUTE
